6535fb5dc08830655e9695865aa41a1b0b3855e8
Marcell Szegedi <marcszeg27@gmail.com>
2021-01-13 15:54:29 +0100

    pvp start screeen added

---
+++ Torpedo/View/pvp_view/MultiPlayer.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Torpedo.View.pvp_view;
+
+namespace Torpedo
+{
+    /// <summary>
+    /// Interaction logic for MultiPlayer.xaml
+    /// </summary>
+    public partial class MultiPlayer : Window
+    {
+        public MultiPlayer()
+        {
+            InitializeComponent();
+        }
+
+        private void StartClick(object sender, RoutedEventArgs e)
+        {
+            String player1Name = player1NameTB.Text;
+            String player2Name = player2NameTB.Text;
+
+            if (player1Name == "" || player2Name == "")
+            {
+                msg("Be kell írni egy nevet mindenkinek!");
+            }
+            else
+            {
+                PvpShipPlacement pvpShipPlacement = new PvpShipPlacement(player1Name, player2Name);
+                this.Close();
+                PvpShipPlacement.Show();
+            }
+        }
+
+        private void BackClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainwindow = new MainWindow();
+            this.Close();
+            mainwindow.Show();
+        }
+        private void msg(string szoveg)
+        {
+
+            MessageBox.Show(szoveg, "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+        }
+    }
+}
