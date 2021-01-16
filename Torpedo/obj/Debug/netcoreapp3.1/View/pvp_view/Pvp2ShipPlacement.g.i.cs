b8978369e267655d1665607f91e4da65a9268468
Marcell Szegedi <marcszeg27@gmail.com>
2021-01-16 16:56:36 +0100

    save fixed

--- Torpedo/obj/Debug/netcoreapp3.1/View/pvp_view/Pvp2ShipPlacement.g.i.cs
+++ Torpedo/obj/Debug/netcoreapp3.1/View/pvp_view/Pvp2ShipPlacement.g.i.cs
@@ -935,13 +935,13 @@ namespace Torpedo.View.pvp_view {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Torpedo;component/view/pvp_view/pvp2shipplacement.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Torpedo;V1.0.0.0;component/view/pvp_view/pvp2shipplacement.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\View\pvp_view\Pvp2ShipPlacement.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -951,7 +951,7 @@ namespace Torpedo.View.pvp_view {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
